86776a2b184aff3684b6c4360b2557da8f88bd3e
Andre Felipe <afelipes@gmail.com>
2022-10-20 16:38:48 -0300

    feat: :rocket: 1 - Implemente a função read

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        jobs_load = csv.DictReader(file)
+        jobs = []
+        for job in jobs_load:
+            jobs.append(job)
+        return jobs
