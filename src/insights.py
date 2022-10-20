ea42acdb582a780492ced86e70984c7400d82188
Andre Felipe <afelipes@gmail.com>
2022-10-20 20:38:03 -0300

    feat: :rocket: 8 - Implemente a função matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -61,29 +61,15 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if type(salary) != int:
+        raise ValueError("1")
+    elif ("max_salary" or "min_salary") not in job:
+        raise ValueError("2")
+    elif (type(job["max_salary"]) or type(job["min_salary"])) != int:
+        raise ValueError("3")
+    elif job["min_salary"] > job["max_salary"]:
+        raise ValueError("4")
+    return job["max_salary"] >= salary >= job["min_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
