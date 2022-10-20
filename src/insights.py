b13512809b71d11ac5f60e69cca11aaef4fe1e10
Andre Felipe <afelipes@gmail.com>
2022-10-20 18:15:53 -0300

    feat: :rocket: 4 - Implemente a função get_max_salary

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,17 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    all_jobs = read(path)
+    max_salarys = set()
+    for job in all_jobs:
+        if job["max_salary"] != "":
+            try:
+                max_salarys.add(int(job["max_salary"]))
+            except ValueError:
+                pass
+    result = max(max_salarys)
+    print(result)
+    return result
 
 
 def get_min_salary(path):
