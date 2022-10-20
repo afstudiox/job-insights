9ed01d7446aff8662c2c4fc995b3dafe0d3b8847
Andre Felipe <afelipes@gmail.com>
2022-10-20 18:49:04 -0300

    feat: :rocket: 7 - Implemente a função filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_jobs = []
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_jobs.append(job)
+    return filtered_jobs
 
 
 def get_max_salary(path):
