c80e16b82872fe1240e7a17bc75b5e9b7aa20ffb
Andre Felipe <afelipes@gmail.com>
2022-10-21 17:58:06 -0300

    feat: :rocket: 12 - Implemente um teste para a função sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,8 @@
-# from src.sorting import sort_by
+from src.jobs import read
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    results = read("src/jobs.csv")
+    print(results)
+    assert sort_by(results, "min_salary") == 0
