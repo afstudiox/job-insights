6525fff9050146e27efdc8f345e57ab62c38c8e0
Andre Felipe <afelipes@gmail.com>
2023-12-08 08:47:18 -0300

    refactor: :recycle: Code improvements
    Add Error Handling & docstring

--- src/jobs.py
+++ src/jobs.py
@@ -1,13 +1,24 @@
 import csv
+import os
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
+    """
+    Reads a CSV file and returns a list of dictionaries
+
+    :param path: path to the CSV file
+
+    :return: list of dictionaries
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File not found: {path}")
 
     with open(path) as file:
-        jobs_load = csv.DictReader(file)
-        jobs = []
-        for job in jobs_load:
-            jobs.append(job)
-        return jobs
+        try:
+            jobs_load = csv.DictReader(file)
+            jobs = [job for job in jobs_load]
+            return jobs
+        except csv.Error as e:
+            raise ValueError(f"Error parsing CSV file: {e}")
